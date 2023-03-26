471fcf5a675e5b405989391daf6dc2748faba229
Khalid Arab <khaledarab1983@hotmail.com>
2023-03-26 16:29:34 +0200

    fix new_password validation und sicherheit

--- user/new_password.php
+++ user/new_password.php
@@ -1,57 +1,83 @@
 <?php
 include ('../conn/db_conn.php');
 
-// $sql = "SELECT email FROM tbl_user WHERE email = $email";
-// $result = mysqli_query($conn, $sql);
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $code = $_POST['code'];
+    $new_password = $_POST['new_password'];
+    $confirm_password = $_POST['confirm_password'];
+}else{
+    $code = "";
+    $new_password = "";
+    $confirm_password = "";
+}
+
+$errors = [
+    'codeError' => '',
+    'newPassError' => '',
+    'confirmPassError' => '',
+    'otherError' => ''
+];
 
 // Prüfen, ob das Formular gesendet wurde
 if(isset($_POST['confirm_code'])) {
     // Bestätigungscode und neue Passwörter aus dem Formular erhalten
-    $code = $_POST['code'];
-    $new_password = $_POST['new_password'];
-    $confirm_password = $_POST['confirm_password'];
+    
+    if(empty($code)){
+        $errors['codeError'] = '* Bitte geben Sie den Code ein, den Sie per E-Mail erhalten haben.';
+    }
+    if(empty($new_password)){
+        $errors['newPassError'] = '* Bitte geben Sie das Password ein.';
+    }
+    if(empty($confirm_password)){
+        $errors['confirmPassError'] = '* Bitte bestätigen Sie das Password.';
+    }
+    if(!array_filter($errors)){
+        
+        $code =              mysqli_real_escape_string($conn, $_POST['code']);
+        $new_password =      mysqli_real_escape_string($conn, $_POST['new_password']);
+        $confirm_password =  mysqli_real_escape_string($conn, $_POST['confirm_password']);
 
-    // Überprüfen, ob der Code korrekt ist
-    // Hier wird der Code einfach aus der Session-Variable gelesen
-    session_start();
-    $saved_code = $_SESSION['forgot_password_code'];
-    $saved_email = $_SESSION['forgot_password_email'];
 
-    $sql = "SELECT email FROM tbl_user WHERE email = '$saved_email'";
-    $result = $conn->query($sql);
+        // Hier wird der Code einfach aus der Session-Variable gelesen
+        session_start();
+        $saved_code = $_SESSION['forgot_password_code'];
+        $saved_email = $_SESSION['forgot_password_email'];
 
-    if($code == $saved_code) {
-        // Code korrekt, Passwörter überprüfen und aktualisieren
-        if($new_password == $confirm_password) {
-            // Passwörter stimmen überein, aktualisieren in der Datenbank oder einer Datei
-            if($result->num_rows > 0){
-                $sql ="UPDATE tbl_user SET password = ? WHERE email = ?";
-                $stmt = mysqli_prepare($conn, $sql);
-                mysqli_stmt_bind_param($stmt, "ss", $new_password, $saved_email);
-                if(mysqli_stmt_execute($stmt)){
-                    header("Location: u_login.php");
+        $sql = "SELECT email FROM tbl_user WHERE email = '$saved_email'";
+        $result = $conn->query($sql);
+
+        if($code == $saved_code) {
+            // Code korrekt, Passwörter überprüfen und aktualisieren
+            if($new_password == $confirm_password) {
+                // Passwörter stimmen überein, aktualisieren in der Datenbank oder einer Datei
+                if($result->num_rows > 0){
+                    $sql ="UPDATE tbl_user SET password = ? WHERE email = ?";
+                    $stmt = mysqli_prepare($conn, $sql);
+                    mysqli_stmt_bind_param($stmt, "ss", $new_password, $saved_email);
+                    if(mysqli_stmt_execute($stmt)){
+                        header("Location: u_login.php");
+                    }else{
+                        echo "Error: " . "<br>" . mysqli_error($conn);
+                    }
+                    // Session-Variable für den Code und die E-Mail-Adresse löschen
+                    unset($_SESSION['forgot_password_code']);
+                    unset($_SESSION['forgot_password_email']);
                 }else{
-                    echo "Error: " . "<br>" . mysqli_error($conn);
+                    $errors['otherError'] = "Die eingegebene Email-Adresse Existiert nicht";
                 }
-                // Session-Variable für den Code und die E-Mail-Adresse löschen
-                unset($_SESSION['forgot_password_code']);
-                unset($_SESSION['forgot_password_email']);
-            }else{
-                echo "Die eingegebene Email-Adresse Existiert nicht";
+            } else {
+                // Passwörter stimmen nicht überein, Fehlermeldung anzeigen
+                $errors['otherError'] = "Die Passwörter stimmen nicht überein!";
             }
-            // // Hier wird das neue Passwort einfach auf der Seite angezeigt
-            // echo "Das neue Passwort für die E-Mail-Adresse $saved_email lautet: $new_password";
         } else {
-            // Passwörter stimmen nicht überein, Fehlermeldung anzeigen
-            echo "Die Passwörter stimmen nicht überein!";
+            // Code falsch, Fehlermeldung anzeigen
+            $errors['otherError'] = "Der eingegebene Code ist falsch!";
         }
-    } else {
-        // Code falsch, Fehlermeldung anzeigen
-        echo "Der eingegebene Code ist falsch!";
     }
 }
 
-// HTML-Formular für die Bestätigung des Codes und die Eingabe der neuen Passwörter
 ?>
 
 
@@ -75,18 +101,22 @@ if(isset($_POST['confirm_code'])) {
             <div class="form_login">
                 <form method="post">
                     <div class="form-floating m-4">
-                        <input type="number" class="form-control" id="code" name="code" placeholder="Bestätigungscode" required>
+                        <input type="number" class="form-control" id="code" name="code" placeholder="Bestätigungscode" value="<?php echo $code ?>">
                         <label for="code">Bestätigungscode</label>
+                        <div class="form-text error"><?php echo $errors['codeError'] ?></div>
                     </div>
 
                     <div class="form-floating m-4">
-                        <input type="password" class="form-control" id="new_password" name="new_password" placeholder="Neues Passwort" required>
+                        <input type="password" class="form-control" id="new_password" name="new_password" placeholder="Neues Passwort" value="<?php echo $new_password ?>">
                         <label for="new_password">Neues Passwort</label>
+                        <div class="form-text error"><?php echo $errors['newPassError'] ?></div>
                     </div>
 
                     <div class="form-floating m-4">
-                        <input type="password" class="form-control" id="confirm_password" name="confirm_password" placeholder="Passwort bestätigen" required>
+                        <input type="password" class="form-control" id="confirm_password" name="confirm_password" placeholder="Passwort bestätigen" value="<?php echo $confirm_password ?>">
                         <label for="confirm_password">Passwort bestätigen</label>
+                        <div class="form-text error"><?php echo $errors['confirmPassError'] ?></div>
+                        <div class="form-text error"><?php echo $errors['otherError'] ?></div>
                     </div>
                     <button type="submit"  class="btn btn-warning m-2 w-25" name="confirm_code" >Bestätigen</button>
                 </form>
