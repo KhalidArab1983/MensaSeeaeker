3fceaa82974c6bb1dd3b00f1de96c9bcd41d66f5
Khalid Arab <khaledarab1983@hotmail.com>
2023-03-29 16:36:41 +0200

    validation und sicherheit für admin register

--- admin/a_register.php
+++ admin/a_register.php
@@ -5,20 +5,56 @@ if ($_SERVER["REQUEST_METHOD"]=="POST"){
     $adminName = $_POST['adminName'];
     $email = $_POST['email'];
     $password = $_POST['password'];
+    $confirm = $_POST['confirm'];
 }else{
     $adminName = "";
     $email = "";
     $password = "";
+    $confirm = "";
 }
 
+$errors = [
+    'adminNameError' => '',
+    'emailError' => '',
+    'passwordError' => '',
+    'confirmError' => '',
+    'otherError' => ''
+];
+
+
 if (isset($_POST['submit'])){
-    $sql = "INSERT INTO tbl_admin (adminName, email, password) VALUES ('$adminName', '$email', '$password')";
-    if(mysqli_query($conn, $sql)){
-        header("Location: a_login.php");
-    }else{
-        echo "Error: " . "<br>" . mysqli_error($conn);
-	}
-	mysqli_close($conn);
+    if(empty($adminName)){
+        $errors['adminNameError'] = '* Bitte geben Sie einen Adminname ein';
+    }
+    if(empty($email)){
+        $errors['emailError'] = '* Bitte geben Sie die E-mail ein';
+    }elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $errors['emailError'] = '* Bitte geben Sie eine gültige Email-Adresse ein';
+    }
+    if(empty($password)){
+        $errors['passwordError'] = '* Bitte geben Sie das Passwort ein';
+    }
+    if(empty($confirm)){
+        $errors['confirmError'] = '* Bitte bestätigen Sie das Passwort';
+    }elseif($password != $confirm){
+        $errors['confirmError'] = 'Die Passwörter stimmen nicht überein!';
+    }
+    if(!array_filter($errors)){
+
+        $adminName =     mysqli_real_escape_string($conn, $_POST['adminName']);
+        $email =        mysqli_real_escape_string($conn, $_POST['email']);
+        $password =        mysqli_real_escape_string($conn, $_POST['password']);
+        $confirm =        mysqli_real_escape_string($conn, $_POST['confirm']);
+
+        $sql = "INSERT INTO tbl_admin (adminName, email, password) VALUES ('$adminName', '$email', '$password')";
+        if(mysqli_query($conn, $sql)){
+            header("Location: a_login.php");
+        }else{
+            echo "Error: " . "<br>" . mysqli_error($conn);
+        }
+        mysqli_close($conn);
+    }
+    
     
     
 }
@@ -65,16 +101,24 @@ if (isset($_POST['submit'])){
             <h4 class="text-center">Admin Konto erstellen</h4>
             <form action="a_register.php" method="post">
                 <div class="form-floating m-5">
-                    <input type="text" class="form-control" name="adminName" id="adminName" placeholder="Admin Name" required>
+                    <input type="text" class="form-control" name="adminName" id="adminName" placeholder="Admin Name" value="<?php echo $adminName ?>">
                     <label for="adminName">Admin Name<i class="mdi mdi-near-me:"></i></label>
+                    <div class="form-text error"><?php echo $errors['adminNameError'] ?></div>
                 </div>
                 <div class="form-floating m-5">
-                    <input type="email" class="form-control" name="email" id="email" placeholder="name@example.com" required>
+                    <input type="text" class="form-control" name="email" id="email" placeholder="name@example.com" value="<?php echo $email ?>">
                     <label for="email">Email adresse</label>
+                    <div class="form-text error"><?php echo $errors['emailError'] ?></div>
                 </div>
                 <div class="form-floating m-5">
-                    <input type="password" class="form-control" name="password" id="password"  placeholder="Kennwort" required>
+                    <input type="password" class="form-control" name="password" id="password"  placeholder="Kennwort" value="<?php echo $password ?>">
                     <label for="password">Kennwort</label>
+                    <div class="form-text error"><?php echo $errors['passwordError'] ?></div>
+                </div>
+                <div class="form-floating m-5">
+                    <input type="password" class="form-control" name="confirm" id="confirm"  placeholder="Kennwort Bestätigung" value="<?php echo $confirm ?>">
+                    <label for="confirm">Kennwort Bestätigung</label>
+                    <div class="form-text error"><?php echo $errors['confirmError'] ?></div>
                 </div>
                 <div class="text-center">
                     <input type="submit" name="submit" class="btn btn-primary m-5 w-25" value="Register">
