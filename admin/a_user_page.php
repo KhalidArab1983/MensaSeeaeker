637591fa082b0a7e3b7881439e1dbba1458f1771
Khalid Arab <khaledarab1983@hotmail.com>
2023-04-18 17:02:14 +0200

    fix comments

--- admin/a_user_page.php
+++ admin/a_user_page.php
@@ -276,7 +276,7 @@ if(isset($_SERVER['REQUEST_METHOD']) && $_SERVER["REQUEST_METHOD"] == "GET"){
                             <tbody>
                                 <?php
                                     $woche_count = ($current_day == $sonntag)?$week_count = date('W') + 1 : $week_count = date('W'); 
-                                    // Abrufen aller Bestellungen aus der Datenbank
+                                    // Abrufen Bestellungen der Benutzern für nächste Woche aus der Datenbank
                                     // $sql = "SELECT u.id, u.userName, b.id, b.option_name, b.option_id, b.day, b.day_datum, b.bestelldatum
                                     //         FROM tbl_user u JOIN (SELECT id, user_id, option_name, option_id, day, day_datum, bestelldatum
                                     //             FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY bestelldatum DESC) AS row_num FROM tbl_bestellung) t
@@ -321,7 +321,7 @@ if(isset($_SERVER['REQUEST_METHOD']) && $_SERVER["REQUEST_METHOD"] == "GET"){
                             <tbody>
                                 <?php
                                     $woche_count = ($current_day == $sonntag)?$week_count = date('W') + 1 : $week_count = date('W'); 
-                                    // Abrufen aller Bestellungen aus der Datenbank
+                                    // Abrufen die Summe der Anzahl jedes Gerichts für die Woche
                                     // $sql = "SELECT b.option_id, b.option_name, COUNT(*) AS anzahl
                                     //         FROM (
                                     //             SELECT option_id, user_id, option_name, ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY bestelldatum DESC) AS row_num
