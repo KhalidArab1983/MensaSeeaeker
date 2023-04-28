6818deb5ac807b8edb4842b0895524e2e7c7d260
Khalid Arab <khaledarab1983@hotmail.com>
2023-04-28 10:39:48 +0200

    font style

--- admin/a_user_page.php
+++ admin/a_user_page.php
@@ -64,6 +64,7 @@ if(isset($_SERVER['REQUEST_METHOD']) && $_SERVER["REQUEST_METHOD"] == "GET"){
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
         <link href="../css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="../css/style.css">
         <title>Mensa</title>
--- admin/meals_edit.php
+++ admin/meals_edit.php
@@ -29,6 +29,7 @@ $conn->close();
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
         <link rel="stylesheet" href="../css/bootstrap.min.css">
         <link rel="stylesheet" href="../css/style.css">
         <title>Mensa</title>
